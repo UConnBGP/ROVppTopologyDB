d80cdf5fdd7f52efac74a186319f2bee57b66cb7
Reynaldo Morillo <reymorillo2@gmail.com>
2019-06-05 10:41:55 -0400

    appended rovpp_ to all tables, so we can use in AHLocal

--- database_structure.sql
+++ database_structure.sql
@@ -1,31 +1,31 @@
-CREATE TABLE mrt_announcements (
+CREATE TABLE rovpp_mrt_announcements (
   origin BIGINT,
   as_path BIGINT[],
   prefix CIDR
 );
-CREATE TABLE roas (
+CREATE TABLE rovpp_roas (
   asn BIGINT,
   prefix INET,
   max_length INTEGER
 );
-CREATE TABLE peers (
+CREATE TABLE rovpp_peers (
   peer_as_1 BIGINT,
   peer_as_2 BIGINT
 );
-CREATE TABLE customer_providers (
+CREATE TABLE rovpp_customer_providers (
   provider_as BIGINT,
   customer_as BIGINT
 );
-CREATE TABLE stubs (
+CREATE TABLE rovpp_stubs (
   stub_asn BIGINT PRIMARY KEY,
   parent_asn BIGINT
 );
-CREATE TABLE extrapolation_results (
+CREATE TABLE rovpp_extrapolation_results (
   asn BIGINT PRIMARY KEY,
   prefix CIDR,
   origin BIGINT,
   received_from_asn BIGINT
 );
-CREATE TABLE rov_ases (
+CREATE TABLE rovpp_rov_ases (
   asn BIGINT
 );
